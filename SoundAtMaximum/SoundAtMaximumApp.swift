46bbae5da52e5a41facc676f91bc2a198f72aa2f
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-21 11:41:05 +0300

    Initial Commit

---
+++ SoundAtMaximum/SoundAtMaximumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundAtMaximumApp.swift
+//  SoundAtMaximum
+//
+//  Created by jopootrivatel on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SoundAtMaximumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
