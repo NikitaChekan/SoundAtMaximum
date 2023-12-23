a9f5b10679ab25a5e2f38f0f3983cc292c5bace4
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-23 21:57:13 +0300

    bug fix

--- SoundAtMaximum/VolumeSliderView.swift
+++ SoundAtMaximum/VolumeSliderView.swift
@@ -9,56 +9,73 @@ import SwiftUI
 
 struct VolumeSliderView: View {
     
-    @State private var sliderPosition: CGFloat = 0
-    @State private var previousSliderPosition: CGFloat = 0
-    @State private var sliderScale: CGFloat = 0
+    @State var value = 0.5
+    
+    @State private var isTouching = false
+    @State private var startValue: CGFloat = 0
+    
+    @State private var hScale: CGFloat = 1
+    @State private var vScale: CGFloat = 1
+    @State private var offset: CGFloat = 0
+
+    let shapeSize = CGSize(width: 82, height: 180)
     
     var body: some View {
-        
-        let sliderHeight: CGFloat = 200
-        let sliderWidth: CGFloat = 100
-        let maxSliderScale: CGFloat = 0.1
-        
-        Rectangle()
-            .frame(width: sliderWidth, height: sliderHeight)
-            .background(.ultraThinMaterial)
-            .gesture(
-                DragGesture()
-                    .onChanged { proxy in
-                        let height = (sliderHeight / 2) - proxy.translation.height
-                        sliderPosition = min(sliderHeight, max(0, height))
-                        
-                        withAnimation {
-                            sliderScale = height > sliderHeight
-                            ? min(maxSliderScale, 1 - sliderHeight / height)
-                            : height < 0
-                            ? -min(maxSliderScale, 1 - sliderHeight / (sliderHeight + abs(height)))
-                            : 0
-                        }
-                    }
-                    .onEnded { _ in
-                        previousSliderPosition = sliderPosition
-                        sliderScale = 0
-                    }
-            )
-            .overlay(alignment: .bottom) {
+        ZStack {
+            GeometryReader { proxy in
                 Rectangle()
-                    .frame(height: sliderPosition)
-                    .foregroundColor(.white)
+                    .background(.ultraThinMaterial)
+                    .environment(\.colorScheme, .light)
+                VStack {
+                    Spacer(minLength: 0)
+                    Rectangle()
+                        .frame(height: value * proxy.size.height)
+                        .foregroundColor(.white)
+                }
             }
-            .mask(RoundedRectangle(cornerRadius: 25))
-            .onAppear() {
-                sliderPosition = sliderHeight / 2
-                previousSliderPosition = sliderHeight / 2
-            }
-            .scaleEffect(
-                CGSize(
-                    width: 1.0 - abs(sliderScale) / 2,
-                    height: 1.0 + abs(sliderScale)
-                ),
-                anchor: sliderScale >= 0 ? .bottom : .top
-            )
-        
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+        .frame(width: shapeSize.width, height: shapeSize.height)
+        .offset(y: offset)
+        .scaleEffect(x: hScale, y: vScale)
+        .gesture(
+            DragGesture(minimumDistance: 0)
+                .onChanged { drag in
+                    if !isTouching {
+                        startValue = value
+                    }
+                    isTouching = true
+                    
+                    let value = startValue - (drag.translation.height / shapeSize.height)
+                    self.value = min(max(value,0), 1)
+                    
+                    var scale: CGFloat = 1
+                    var offset: CGFloat = 0
+                    
+                    if value > 1 {
+                        scale = sqrt(sqrt(sqrt(value)))
+                        offset = shapeSize.height * (1 - scale)
+                    } else if value < 0 {
+                        scale = sqrt(sqrt(sqrt(1 - value)))
+                        offset = -shapeSize.height * (1 - scale)
+                    } else {
+                        scale = 1
+                    }
+                    
+                    vScale = scale
+                    hScale = 2 - scale
+                    
+                    self.offset = offset
+                }
+                .onEnded { _ in
+                    isTouching = false
+                    withAnimation {
+                        vScale = 1
+                        hScale = 1
+                        offset = 0
+                    }
+                }
+        )
     }
 }
 
